71f6e3239c2d61f3f0e690c625cc52d9ac5150d9
hurgiles <yeiden1481@gmail.com>
2024-08-07 01:19:06 -0500

    cambio

--- scripts.js
+++ scripts.js
@@ -4,28 +4,28 @@ const movies = [
         time: "18:00",
         location: "Sala 1",
         description: "Descripción de la película 1.",
-        poster: "https://via.placeholder.com/200x300"
+        poster: "img/imagen1.png"
     },
     {
         title: "Película 2",
         time: "20:00",
         location: "Sala 2",
         description: "Descripción de la película 2.",
-        poster: "https://via.placeholder.com/200x300"
+        poster: "img/imagen2.png"
     },
     {
         title: "Película 3",
         time: "22:00",
         location: "Sala 3",
         description: "Descripción de la película 3.",
-        poster: "https://via.placeholder.com/200x300"
+        poster: "img/imagen3.png"
     },
     {
         title: "Película 4",
         time: "00:00",
         location: "Sala 4",
         description: "Descripción de la película 4.",
-        poster: "https://via.placeholder.com/200x300"
+        poster: "img/imagen4.png"
     }
 ];
 
